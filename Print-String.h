ba02f777ab7c33eda6376fe17de2bfae9579285b
lavandesh <31106149+lavandesh@users.noreply.github.com>
2020-08-26 12:34:28 +0530

    Update Print-String.h

--- Print-String.h
+++ Print-String.h
@@ -1,12 +1,12 @@
 #include "TelcoFun.h"
 namespace TelCoColorCoder
 {
-  string ToString()
+  void ToString()
   {
     string manual="";
     for (int i = 0; i<26; i++)
     {
       manual += GetColorFromPairNumber(i).Tostring();
     }
-    return manual;
+    cout << manual << endl;
 }
